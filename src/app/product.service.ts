46296cf0fdef19eb7afb8d1dff5b9d681485d075
Dhaval Patel <>
2019-01-09 17:15:30 +0530

    response

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -10,7 +10,8 @@ export class ProductService {
   constructor(private _http: Http) {}
 
   getAlbum(id: number): Observable<Album> {
-    return this._http.get(this._albumUrl).pipe(map(response =>
-      (<Album>response.json())));
+    return this._http.get(this._albumUrl).pipe(map(response => {
+      return <Album>response.json();
+    }));
   }
 }
